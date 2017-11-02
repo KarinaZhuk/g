21bf4168f0bd34945fdc198e9bc43d66dc2e002a
ikoleda <33323309+ikoleda@users.noreply.github.com>
2017-11-02 21:16:05 +0300

    dg

---
+++ трпо/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+namespace трпо
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader("text.txt"))
+            {
+                
+                    string a = sr.ReadToEnd();
+                    Match match = Regex.Match(a, @"\d\d[.]\d\d[.]\d\d\d\d");
+                    if (match.Success)
+                    {
+                        using (StreamWriter sn = new StreamWriter("text1.txt"))
+                        {
+                        sn.WriteLine(match.Captures[0].Value);
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
+
